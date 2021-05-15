0738a3178c6fbf78101b3e75064f42fdce272a3d
Erica Carvalho <ericamcar28@gmail.com>
2021-05-16 00:22:05 +0100

    bootcamp model

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcampRouter = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/database');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Route files
+const bootcampRouter = require('./routes/bootcamps');
+
+// Connect database
+connectDB();
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan
+      .inverse
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
