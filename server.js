0be25b2cddb403507b205aabd9bc0cb885133d44
Erica Carvalho <ericamcar28@gmail.com>
2021-05-16 16:21:38 +0100

    add error handler middleware

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/database');
 const colors = require('colors');
+const connectDB = require('./config/database');
+const errorHandler = require('./middleware/error');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -25,6 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcampRouter);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
